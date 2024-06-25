decb6448e336ec9a5025af2ec2b16ef7c85cf0bc
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-06-25 20:47:44 +0300

    Update dashboard.blade.php

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,29 @@
-<div>
-    <x-mary-main full-width>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="retro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <style>
+    </style>    
+</head>
+
+
+<body>
+    <div>
+        <x-mary-main full-width>
             {{-- SIDEBAR --}}
             <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
-     
-     
                 {{-- MENU --}}
                 <x-mary-menu activate-by-route>
  
@@ -23,16 +43,18 @@
                     <x-mary-menu-item title="History" icon="o-clock" link="#" />
                     <x-mary-menu-item title="Donate" icon="o-gift" link="#" />
                     <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
-                        <x-mary-menu-item title="Log out" icon="o-power" link="/logout" />
-                        <x-mary-menu-item title="Change Theme" icon="o-moon">
-                            <x-mary-theme-toggle darkTheme="coffee" lightTheme="bumblebee" />
-                        </x-mary-menu-item>
+                    <x-mary-menu-item title="Log out" icon="o-power" link="/logout" />
+                    <x-mary-menu-item title="Change Theme" icon="o-moon">
+                    <x-mary-theme-toggle darkTheme="coffee" lightTheme="bumblebee" />
+                    </x-mary-menu-item>
                     </x-mary-menu-sub>
                 </x-mary-menu>
             </x-slot:sidebar>
      
+
             {{-- The `$slot` goes here --}}
             <x-slot:content>
+                
             <x-mary-form wire:submit.prevent="save">
                 <x-mary-header title="Donate" with-anchor separator />
                 <x-mary-input label="Item" placeholder="Enter item" wire:model="name" />
@@ -63,33 +85,10 @@
                 
                 require __DIR__.'/auth.php'; />
                 </x-slot:actions>
-
-                {{-- <x-mary-dropdown label="Settings" class="btn-outline">
-                    
-                    <x-mary-menu-item title="Close after click" />
-                 
-                    <x-mary-menu-separator />
-                 
-                    
-                    <x-mary-menu-item title="Keep open after click" @click.stop="alert('Keep open')" />
-                 
-                    
-                    <x-mary-menu-item title="Call wire:click" wire:click.stop="delete" />
-                 
-                    <x-mary-menu-separator />
-                 
-                    <x-mary-menu-item @click.stop="">
-                        <x-mary-checkbox label="Activate" />
-                    </x-mary-menu-item>
-                 
-                    <x-mary-menu-item @click.stop="">
-                        <x-mary-toggle label="Sleep mode" right />
-                    </x-mary-menu-item>
-                </x-mary-dropdown> 
-
-                --}}
             </x-mary-form>
             </x-slot:content>
         </x-mary-main>
     </div>
-    
+
+</body>
+</html>
