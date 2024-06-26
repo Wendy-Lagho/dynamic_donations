f3f3aa505c74225bc3260ed2c695d4839eb9b947
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-06-26 21:41:28 +0300

    Create 2024_06_26_093841_create_needs_table.php

---
+++ database/migrations/2024_06_26_093841_create_needs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+                // Drop the table if it exists
+                Schema::dropIfExists('needs');
+
+                
+        Schema::create('needs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('need_name', 100); // Specify maximum length for consistency
+            $table->unsignedInteger('quantity_required'); // Ensure only positive values
+            $table->boolean('fulfilled')->default(false); // Boolean field to indicate if the need is fulfilled
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('needs');
+    }
+};
