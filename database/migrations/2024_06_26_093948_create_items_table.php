49c26c8106ad7dad83a3bb4bd60a6f2380b68d1b
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-06-29 22:17:55 +0300

    update database migrations

--- database/migrations/2024_06_26_093948_create_items_table.php
+++ database/migrations/2024_06_26_093948_create_items_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-                // Drop the table if it exists
-                Schema::dropIfExists('items');
+        // Drop the table if it exists
+        Schema::dropIfExists('items');
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
